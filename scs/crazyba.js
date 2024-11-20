4350dfe111fb3613d3847c20ac58ce64701ca6f7
abdallahsalimjuma <abdallahsalimjuma35@gmail.com>
2024-11-20 12:12:35 +0300

    Update and rename bmwba.js to crazyba.js

--- scs/crazyba.js
+++ scs/crazyba.js
@@ -2,9 +2,9 @@
 Yoo bro what au looking for
 Just deploy the bot with original repo
 
-BMW MD
+CRAZY BOT
 
-Made by dev Ibrahim Adams
+Made by dev Dullah Md
 
 
                  _.u[[/;:,.         .odMMMMMM'
